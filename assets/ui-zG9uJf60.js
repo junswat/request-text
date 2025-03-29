e1dc65a8cdf194b2730f231bd37256a9289676fb
junswat <junswat@users.noreply.github.com>
2025-03-29 15:32:30 +0000

    deploy: 9e9a6052d5f3c8c1d03653f5825ed2b377021069

---
+++ assets/ui-zG9uJf60.js
@@ -0,0 +1,13 @@
+import{r as e}from"./vendor-B6OWX3Rw.js";
+/**
+ * @license lucide-react v0.344.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var r={xmlns:"http://www.w3.org/2000/svg",width:24,height:24,viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:2,strokeLinecap:"round",strokeLinejoin:"round"};
+/**
+ * @license lucide-react v0.344.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=((t,o)=>{const a=e.forwardRef((({color:a="currentColor",size:s=24,strokeWidth:i=2,absoluteStrokeWidth:n,className:d="",children:l,...c},h)=>{return e.createElement("svg",{ref:h,...r,width:s,height:s,stroke:a,strokeWidth:n?24*Number(i)/Number(s):i,className:["lucide",`lucide-${m=t,m.replace(/([a-z0-9])([A-Z])/g,"$1-$2").toLowerCase().trim()}`,d].join(" "),...c},[...o.map((([r,t])=>e.createElement(r,t))),...Array.isArray(l)?l:[l]]);var m}));return a.displayName=`${t}`,a})("Loader2",[["path",{d:"M21 12a9 9 0 1 1-6.219-8.56",key:"13zald"}]]);export{t as L};
